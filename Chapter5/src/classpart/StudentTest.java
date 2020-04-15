2bc2dc67346ef21c36b13a65ace81116568f5b6b
Minhyeon Lee <minhyeonlee1@gmail.com>
2020-04-15 14:47:02 +0900

    Chapter5-3. Instance, StudentTest.java modified

--- Chapter5/src/classpart/StudentTest.java
+++ Chapter5/src/classpart/StudentTest.java
@@ -10,6 +10,17 @@ public class StudentTest {
 		studentLee.address = "서울";
 
 		studentLee.showStudentInfo();
+
+		// studentKim 추가
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.address = "경주";
+
+		studentKim.showStudentInfo();
+
+		// 참조 변수의 값을 출력
+		System.out.println(studentLee); // classpart.Student@5ebec15
+		System.out.println(studentLee); // classpart.Student@5ebec15
 	}
 
 }
